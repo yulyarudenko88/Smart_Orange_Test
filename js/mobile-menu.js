984d83e0ac93fa4aeabffdbea7a74148aa4866dd
yulyarudenko88 <111283521+yulyarudenko88@users.noreply.github.com>
2023-08-13 18:32:59 +0300

    ADD: mobile menu

---
+++ js/mobile-menu.js
@@ -0,0 +1,20 @@
+(() => {
+  const menuBtn = document.querySelector("[data-menu-button]");
+  const mobileMenu= document.querySelector("[data-mobile-menu]");
+
+  menuBtn.addEventListener("click", () => {
+    const expanded =
+      menuBtn.getAttribute("aria-expanded") === "true";
+
+    menuBtn.classList.toggle("is-open");
+    menuBtn.setAttribute("aria-expanded", !expanded); 
+    mobileMenu.classList.toggle("is-open");
+
+    window
+    .matchMedia('(min-width: 600px)')
+    .addEventListener(
+      'change',
+      e => e.matches && mobileMenu.classList.remove('is-open'),
+    );
+  });
+})();
