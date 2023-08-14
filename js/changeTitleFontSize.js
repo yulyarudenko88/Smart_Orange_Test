dadf27d73f29fcc6cfffd4096e95456559a167f9
yulyarudenko88 <111283521+yulyarudenko88@users.noreply.github.com>
2023-08-14 19:01:07 +0300

    UPDATE: adaptive and script for _projects-section

---
+++ js/changeTitleFontSize.js
@@ -0,0 +1,12 @@
+  window.addEventListener("load", changeTitleFontSize);
+  window.addEventListener("resize", changeTitleFontSize);
+
+  function changeTitleFontSize() {
+    const overlayElements = document.querySelectorAll(".overlay");
+    overlayElements.forEach((overlay) => {
+      const overlayTitle = overlay.querySelector(".overlay-title");
+      const overlayWidth = overlay.offsetWidth;
+      const fontSize = overlayWidth * 0.1;
+      overlayTitle.style.fontSize = `${fontSize}px`;
+    });}
+  
