be2c2a9d29f3379ddd3c446dd99390f100f80ad1
yulyarudenko88 <111283521+yulyarudenko88@users.noreply.github.com>
2023-08-14 16:44:42 +0300

    ADD: slider js and styles

---
+++ js/slider.js
@@ -0,0 +1,60 @@
+const slides = document.querySelectorAll(".slide");
+const nextSlide = document.querySelector(".btn-next");
+const prevSlide = document.querySelector(".btn-prev");
+const paginationItems = document.querySelectorAll(".pagination-item");
+
+let curSlide = 0;
+let maxSlide = slides.length - 1;
+
+slides.forEach((slide, index) => {
+  slide.style.transform = `translateX(${index * 100}%)`;
+});
+
+nextSlide.addEventListener("click", () => {
+  curSlide += 1;
+
+  slides.forEach((slide, index) => {
+    slide.style.transform = `translateX(${100 * (index - curSlide)}%)`;
+    console.log(curSlide);
+  });
+
+  updateButtonState();
+  updateActivePagination();
+});
+
+prevSlide.addEventListener("click", () => {
+  curSlide -= 1;
+
+  slides.forEach((slide, index) => {
+    slide.style.transform = `translateX(${100 * (index - curSlide)}%)`;
+  });
+
+  updateButtonState();
+  updateActivePagination();
+});
+
+function updateButtonState() {
+  if (curSlide === maxSlide) {
+    nextSlide.disabled = true;
+  } else {
+    nextSlide.disabled = false;
+  }
+
+  if (curSlide === 0) {
+    prevSlide.disabled = true;
+  } else {
+    prevSlide.disabled = false;
+  }
+}
+
+function updateActivePagination() {
+  paginationItems.forEach((item, index) => {
+    if (index === curSlide) {
+      item.classList.add("pagination-item--active");
+    } else {
+      item.classList.remove("pagination-item--active");
+    }
+  });
+}
+
+updateActivePagination();
